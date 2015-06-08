18cdc9ac69e318304ac904b20d2d4aea8ae4ed6d
IanBaldo <ianblopes@gmail.com>
2015-06-08 13:52:07 -0300

    added signal handler

--- tossam/receivData.lua
+++ tossam/receivData.lua
@@ -1,7 +1,11 @@
-local tossam = require("tossam") 
+local tossam = require("tossam")
+local sig = require("posix.signal")
 
--- DATA FILE
-file = io.open("data.txt","w+")
+-- Data Table
+data = {}
+
+period = 10
+currentPeriod = math.floor(os.time()/period)
 
 Tadjust={
 [2]={offset=2.59461, alpha=0.68592, tp='MTS300A'},
@@ -11,6 +15,19 @@ Tadjust={
 [6]={offset=1.82639, alpha=0.72983, tp='MTS300B'},
 [8]={offset=4.46287, alpha=0.64626, tp='MTS300B'},
 [11]={offset=3.74651, alpha=0.65442, tp='MTS300B'},
+}
+
+Tsalas = {
+[2]="Sala 502",
+[3]="Sala 503",
+[4]="Sala 504",
+[5]="Sala 505",
+[6]="Sala 506",
+[7]="Sala 507",
+[8]="Sala 508",
+[9]="Sala 509",
+[10]="Sala 510",
+[11]="Sala 511",
 }
 
 function convertDA(ADC,tipo,mote)
@@ -45,7 +62,41 @@ function round5(num)
 	end
 end
 
-while (1) do
+function fText(str,tam)
+	tmp = str .. string.rep(" ",tam)
+	return string.sub(tmp,1,tam)
+end
+
+function geraArq()
+	-- DATA FILE
+	file = io.open("data.txt","w+")
+	file:write("\nPUC-Rio -- Departamento de Informática \t" .. os.date("%x %X") .. "\n\n")
+	file:write("============  Monitoramento de temperatura  =============\n\n") 
+	file:write(fText("Local",25)..fText("Temp(ºC)",15)..fText(" Data/Hora",17).."\n")
+	for i=2,20 do
+		if data[i] then
+			file:write(fText(Tsalas[data[i].nodeId],25) .. fText(data[i].TempC,15) .. fText(data[i].date,17).."\n")
+		end
+	end
+	file:write("\n=========================================================\n\n")
+	file:close()
+end
+
+function checaPeriodo()
+	testPeriod = math.floor(os.time()/period)
+	if testPeriod > currentPeriod then
+		geraArq()
+		currentPeriod = testPeriod
+	end
+end
+
+function handler()
+	geraArq()
+	print("\nbye bye\n")	
+	os.exit()
+end
+
+while (1) do	
 
 	local mote = tossam.connect("sf@localhost:9002",1)
 --	local micaz = tossam.connect("serial@/dev/ttyUSB1:micaz",1)
@@ -67,16 +118,20 @@ while (1) do
 		]]
 
 		print("msgID","nodeID","seq","temp","date")
-		file:write("nodeID\ttemp\tdate\n")
+	--	file:write("nodeID\ttemp\t\tdate\n")
 		while (mote) do
+			sig.signal(sig.SIGINT,handler)
+			sig.signal(sig.SIGTERM,handler)
 
 			local stat, msg, emsg = pcall(function() return mote:receive() end) 
 			if stat then
 				if msg then
+					checaPeriodo()
 					local date = os.date("%x %X")
 					local TempC = convertDA(msg.temp,Tadjust[msg.nodeId].tp,msg.nodeId)
+					data[msg.nodeId] = {nodeId = msg.nodeId, seq=msg.seq, TempC=string.format("%.1f", round5(TempC)), date=date}
 					print(msg.id,msg.nodeId,msg.seq,string.format("%.1f", round5(TempC)),date)
-					file:write(msg.nodeId,"\t\t" .. string.format("%.1f", round5(TempC)),"\t\t" .. date,"\n")
+					--file:write(msg.nodeId,"\t\t" .. string.format("%.1f", round5(TempC)),"\t\t" .. date,"\n")
 				else
 					if emsg == "closed" then
 						print("\nConnection closed!")
@@ -89,10 +144,9 @@ while (1) do
 				break
 			end
 		end
-
+		
 		mote:unregister()
 		mote:close()
-		file:close()
 	end
 	
 	os.execute("sleep " .. 5)
